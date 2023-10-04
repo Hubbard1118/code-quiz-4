f9e5e073a18ef9e9dc6070d7d8f576571dc564ee
Yasin Hubbard <you@example.com>
2023-10-03 23:29:05 -0400

    questions and algorithm

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// start by setting the variables to keep track of game state
+// currentQuesiton
+// timeRemaining
+
+
+//  Declare your dom elements such as 
+// submit button dom Element
+// timer Element
+// score
+
+// function to start timer and then call question function to generate the first question and answer buttons.
+
+// function to pull question from questions array and generate buttons with answers by looping over the answer arrays.   
+
+// event function to grab the value of the button clicked and compare to answer.  If same recall question function for the next question, else decrement timer and recall question function for next question
+
+// function to end quiz and capture time remaining as high score.  Store the highscores in local storage.  Dont forget to format the data coming back from high scores in descending order
+
+
+var questions = [
+    {
+        title: 'What is the .log method used for?'
+        choices: ['navigating', 'Speaking to the console', 'Reading code']
+        answer: ''
+    }
+    {
+        title: ""
+        choices:
+        answer:
+    }
+    {
+        title: ""
+        choices:
+        answer:
+    }
+    {
+        title: ""
+        choices:
+        answer:
+    }
+    {
+        title: ""
+        choices:
+        answer:
+    }
+    {
+        title: ""
+        choices:
+        answer:
+    }
+]
